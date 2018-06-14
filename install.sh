b370048570a505743ec1b3c77559b4899b39540c
thorntheletter <thorntheletter@gmail.com>
2018-06-13 23:59:06 -0500

    changed install script to recusively create symlinks in same directory structure as profile, created desktop profile as a copy of laptop for now

--- install.sh
+++ install.sh
@@ -1,25 +1,42 @@
 #!/bin/sh
 
+# Recursively creates symlinks in the same directory structure as in the profile
+symlink_directory()
+{
+    for file in  .* *
+    do
+        if [ $file = "." -o $file = ".." ]
+        then
+            continue
+        elif [ -d $file ]
+        then
+            cd $file
+            INST_DIR=$INST_DIR/$file
+            mkdir -pv $INST_DIR
+            symlink_directory
+            INST_DIR=${INST_DIR%/*}
+            cd ..
+        else
+            ln -si `pwd`/$file $INST_DIR/$file
+        fi
+    done
+}
+
 # Check to make sure it has required argument
 if [ -z "$1" ]
 then
-    echo 'Usage is "./install.sh PROFILE [HOME_DIR]"'
+    echo 'Usage is "./install.sh PROFILE [INSTALL_DIR]"'
     exit 1
 fi
 
-# Set INST_HOME to install home directory if in arguments
+# Set INST_DIR to install home directory if in arguments
 if [ -n "$2" ]
 then 
-    if [ -n "$(echo $2 | grep -E '?/$?')" ]
-    then
-        INST_HOME=$2
-    else
-        INST_HOME=$2/
-    fi
+    INST_DIR=${2%/}
 else
-    INST_HOME="$HOME/"
+    INST_DIR=$HOME
 fi
 
-# Run profile install script
+# Go to specified profile and call symlink_directory
 cd profiles/$1
-. ./install.sh
+symlink_directory
---
+++ profiles/laptop/.bash_profile
@@ -0,0 +1,7 @@
+#
+# ~/.bash_profile
+#
+
+#emacs --daemon &
+
+[[ -f ~/.bashrc ]] && . ~/.bashrc
